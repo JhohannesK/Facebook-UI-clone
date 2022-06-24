050290ce7c15520e9c9c8f8150afe5604b5ce6fd
JhohannesK <Jdomeh77@gmail.com>
2022-06-24 12:04:41 +0000

    Trying medias

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  content: ["./public/**/*.{html, js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('tailwind-scrollbar-hide')
-  ],
-}
+	content: ['./public/**/*.{html, js}'],
+	theme: {
+		screens: {
+			sm: '320px',
+			md: '375',
+			lg: '600',
+		},
+		extend: {},
+	},
+	plugins: [require('tailwind-scrollbar-hide')],
+};
