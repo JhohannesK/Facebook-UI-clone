277be60f621254c21fb759ad4a3d179fb17335b2
jhohannesK <jdomeh77@gmail.com>
2024-01-24 17:00:18 +0000

    start

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 module.exports = {
 	content: ['./public/**/*.{html, js}'],
 	theme: {
-		screens: {
-			sm: '320px',
-			md: '375',
-			lg: '600',
-		},
 		extend: {},
 	},
 	plugins: [require('tailwind-scrollbar-hide')],
